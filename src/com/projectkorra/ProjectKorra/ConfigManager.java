9ff46b446516e24762bf897ccffc27785472077f
nathank33 <nathanbraunguitar@gmail.com>
2014-11-24 23:04:34 -0800

    Twister Particle Config Options

--- src/com/projectkorra/ProjectKorra/ConfigManager.java
+++ src/com/projectkorra/ProjectKorra/ConfigManager.java
@@ -231,6 +231,8 @@ public class ConfigManager {
 		config.addDefault("Abilities.Air.AirCombo.Twister.Radius", 3.5);
 		config.addDefault("Abilities.Air.AirCombo.Twister.RemoveDelay", 1500);
 		config.addDefault("Abilities.Air.AirCombo.Twister.Cooldown", 10000);
+		config.addDefault("Abilities.Air.AirCombo.Twister.DegreesPerParticle", 7);
+		config.addDefault("Abilities.Air.AirCombo.Twister.HeightPerParticle", 1.25);
 		config.addDefault("Abilities.Air.AirCombo.AirStream.Speed", 0.5);
 		config.addDefault("Abilities.Air.AirCombo.AirStream.Range", 40);
 		config.addDefault("Abilities.Air.AirCombo.AirStream.EntityDuration", 4000);
