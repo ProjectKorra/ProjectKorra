7e06692d73c9e71a9cfef06e80f3d3fc03e470c2
MistPhizzle <mistphizzle@gmail.com>
2014-12-01 16:36:02 -0500

    Begin Spigot 1.8 Support
    Uses Spigot's version of CraftBukkit.
    Update methods to include getOnlinePlayers() method.

--- src/com/projectkorra/ProjectKorra/RevertChecker.java
+++ src/com/projectkorra/ProjectKorra/RevertChecker.java
@@ -43,9 +43,8 @@ public class RevertChecker implements Runnable {
 		@Override
 		public ArrayList<Chunk> call() throws Exception {
 			ArrayList<Chunk> chunks = new ArrayList<Chunk>();
-			Player[] players = server.getOnlinePlayers();
-
-			for (Player player : players) {
+			
+			for (Player player : server.getOnlinePlayers()) {
 				Chunk chunk = player.getLocation().getChunk();
 				if (!chunks.contains(chunk))
 					chunks.add(chunk);
