e66aa4224a5d0b377a1e3424f157a41b31272632
Brendan Wilson <belancew@gmail.com>
2015-01-21 15:03:10 -0500

    Fireblast now ignites furnace blocks.
    Fireblast can be used to add burning ticks to a furnace.

--- src/com/projectkorra/ProjectKorra/firebending/FireBlast.java
+++ src/com/projectkorra/ProjectKorra/firebending/FireBlast.java
@@ -126,9 +126,10 @@ public class FireBlast {
 
 		if (Methods.isSolid(block) || block.isLiquid()) {
 			if (block.getType() == Material.FURNACE && canPowerFurnace) {
-				Furnace f = ((Furnace) block);
-				f.setBurnTime((short) 800);
-				f.setCookTime((short) 800);
+				Furnace furnace = (Furnace) block.getState();
+				furnace.setBurnTime((short) 800);
+				furnace.setCookTime((short) 800);
+				furnace.update();
 			} else if (FireStream.isIgnitable(player, block.getRelative(BlockFace.UP))) {
 				ignite(location);
 			}
