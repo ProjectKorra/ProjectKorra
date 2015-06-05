3110b8290e2a4648916acbb0cf4d712871a23872
jedk1 <jedkristian@live.com>
2015-06-06 00:10:29 +0100

    Added config manager to support additional config.

---
+++ src/com/projectkorra/ProjectKorra/configuration/Config.java
@@ -0,0 +1,70 @@
+package com.projectkorra.ProjectKorra.configuration;
+
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import com.projectkorra.ProjectKorra.ProjectKorra;
+
+public class Config {
+
+	ProjectKorra plugin;
+
+	private File file;
+	public FileConfiguration config;
+
+	public Config(File file) {
+		this.plugin = ProjectKorra.plugin;
+		this.file = new File(plugin.getDataFolder() + File.separator + file);
+		this.config = YamlConfiguration.loadConfiguration(this.file);
+		loadConfig();
+	}
+
+	public void createConfig() {
+		if (!file.getParentFile().exists()) {
+			try {
+				file.getParentFile().mkdir();
+				plugin.getLogger().info(
+						"Generating new directory for " + file.getName() + "!");
+			} catch (Exception e) {
+				plugin.getLogger().info("Failed to generate directory!");
+				e.printStackTrace();
+			}
+		}
+
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+				plugin.getLogger().info(
+						"Generating new " + file.getName() + "!");
+			} catch (Exception e) {
+				plugin.getLogger().info(
+						"Failed to generate " + file.getName() + "!");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public FileConfiguration getConfig() {
+		return config;
+	}
+
+	public void loadConfig() {
+		createConfig();
+		try {
+			config.load(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void saveConfig() {
+		try {
+			config.options().copyDefaults(true);
+			config.save(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
