18d39441924e78087387c936edfc20483c5b111c
Jack Lin <jacklin213@gmail.com>
2015-06-06 11:57:09 +1200

    Add ConfigType

---
+++ src/com/projectkorra/ProjectKorra/configuration/ConfigType.java
@@ -0,0 +1,18 @@
+package com.projectkorra.ProjectKorra.configuration;
+
+/**
+ * Enum representing different types of Configurations.
+ * 
+ * @author Jacklin213
+ *
+ */
+public enum ConfigType {
+	/**
+	 * Default Configuration represented by config.yml
+	 */
+	DEFAULT,
+	/**
+	 * Death Message configuration represented by deathmessages.yml
+	 */
+	DEATH_MESSAGE;
+}
