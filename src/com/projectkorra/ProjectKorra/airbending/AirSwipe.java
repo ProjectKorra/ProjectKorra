2745c8b4c4e5c1cac2a026aacb006d2b11908fd2
Jack Lin <jacklin213@gmail.com>
2015-06-07 10:58:18 +1200

    AirSwipe compare distance fix

--- src/com/projectkorra/ProjectKorra/airbending/AirSwipe.java
+++ src/com/projectkorra/ProjectKorra/airbending/AirSwipe.java
@@ -305,7 +305,7 @@ public class AirSwipe {
 			
 			for(Vector vec : aswipe.elements.keySet()) {
 				Location vectorLoc = aswipe.elements.get(vec);
-				if(vectorLoc != null && vectorLoc.getWorld() != loc.getWorld() && vectorLoc.distance(loc) <= radius){
+				if(vectorLoc != null && !vectorLoc.getWorld().equals(loc.getWorld()) && vectorLoc.distance(loc) <= radius){
 					instances.remove(aswipe.id);
 					removed = true;
 				}
