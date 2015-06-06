cd62f2a4c91a542e7425862dfea968f5cf0e42ff
jedk1 <jedkristian@live.com>
2015-06-06 20:10:58 +0100

    Added messages for most abilities.

--- src/com/projectkorra/ProjectKorra/configuration/ConfigManager.java
+++ src/com/projectkorra/ProjectKorra/configuration/ConfigManager.java
@@ -26,6 +26,44 @@ public class ConfigManager {
 			config = deathMsgConfig.getConfig();
 			
 			config.addDefault("Properties.Enabled", true);
+			config.addDefault("Properties.Default", "{victim} was slain by {attacker}'s {ability}");
+			
+			config.addDefault("Air.AirBlast", "{victim} was flung by {attacker}'s {ability}");
+			config.addDefault("Air.AirBurst", "{victim} was thrown down by {attacker}'s {ability}");
+			config.addDefault("Air.AirSwipe", "{victim} was struck by {attacker}'s {ability}");
+			config.addDefault("Air.Suffocate", "{victim} was asphyxiated by {attacker}'s {ability}");
+			config.addDefault("Air.Combo", "{victim} was overwhelmed by {attacker}'s skill {ability}");
+			
+			config.addDefault("Water.IceBlast", "{victim} was shattered by {attacker}'s {ability}");
+			config.addDefault("Water.Torrent", "{victim} was taken down by {attacker}'s {ability}");
+			config.addDefault("Water.IceSpike", "{victim} was impaled by {attacker}'s {ability}");
+			config.addDefault("Water.WaterManipulation", "{victim} was taken down by {attacker}'s {ability}");
+			config.addDefault("Water.WaterArms", "{victim} was too slow for {attacker}'s {ability}");
+			config.addDefault("Water.OctopusForm", "{victim} was slapped by {attacker}'s {ability}");
+			config.addDefault("Water.Bloodbending", "{victim} was destroyed by {attacker}'s {ability}");
+			config.addDefault("Water.Combo", "{victim} was overwhelmed by {attacker}'s skill {ability}");
+			
+			config.addDefault("Earth.Collapse", "{victim} was suffocated by {attacker}'s {ability}");
+			config.addDefault("Earth.EarthBlast", "{victim} was broken apart by {attacker}'s {ability}");
+			config.addDefault("Earth.EarthSmash", "{victim} was crushed by {attacker}'s {ability}");
+			config.addDefault("Earth.LavaFlow", "{victim} was caught in by {attacker}'s {ability}");
+			config.addDefault("Earth.MetalClips", "{victim} was too slow for {attacker}'s {ability}");
+			config.addDefault("Earth.Shockwave", "{victim} was blown away by {attacker}'s {ability}");
+			config.addDefault("Earth.Combo", "{victim} was overwhelmed by {attacker}'s skill {ability}");
+			
+			config.addDefault("Fire.FireBlast", "{victim} was burnt by {attacker}'s {ability}");
+			config.addDefault("Fire.Blaze", "{victim} was burned alive by {attacker}'s {ability}");
+			config.addDefault("Fire.WallOfFire", "{victim} ran into {attacker}'s {ability}");
+			config.addDefault("Fire.Lightning", "{victim} was electrocuted by {attacker}'s {ability}");
+			config.addDefault("Fire.Combustion", "{victim} was shot down by {attacker}'s {ability}");
+			config.addDefault("Fire.FireBurst", "{victim} was blown apart by {attacker}'s {ability}");
+			config.addDefault("Fire.FireShield", "{victim} scorched theirself on {attacker}'s {ability}");
+			config.addDefault("Fire.Combo", "{victim} was overwhelmed by {attacker}'s skill {ability}");
+			
+			config.addDefault("Chi.QuickStrike", "{victim} was struck down by {attacker}'s {ability}");
+			config.addDefault("Chi.SwiftKick", "{victim} was kicked to the floor by {attacker}'s {ability}");
+			config.addDefault("Chi.RapidPunch", "{victim} took all the hits against {attacker}'s {ability}");
+			config.addDefault("Chi.Combo", "{victim} was overwhelmed by {attacker}'s skill {ability}");
 			
 			config.options().copyDefaults(true);
 			deathMsgConfig.saveConfig();
