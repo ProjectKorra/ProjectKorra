459a3fb0a1062c92935f53c72d92566b25937c67
Jack Lin <jacklin213@gmail.com>
2015-06-08 10:58:22 +1200

    Update formatter with month and date

--- src/com/projectkorra/ProjectKorra/Utilities/logging/LogFormatter.java
+++ src/com/projectkorra/ProjectKorra/Utilities/logging/LogFormatter.java
@@ -11,14 +11,16 @@ import java.util.logging.LogRecord;
  */
 public class LogFormatter extends Formatter {
 
-private final SimpleDateFormat date = new SimpleDateFormat("HH:mm:ss");
+private final SimpleDateFormat date = new SimpleDateFormat("MMM-dd|HH:mm:ss");
 	
 	@Override
 	public String format(LogRecord record) {
 		StringBuilder builder = new StringBuilder();
         Throwable ex = record.getThrown();
 
+        builder.append("(");
         builder.append(date.format(record.getMillis()));
+        builder.append(")");
         builder.append(" [");
         builder.append(record.getLevel().getLocalizedName().toUpperCase());
         builder.append("] ");
