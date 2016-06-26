0e9e901dc0baaa655a2b7f1c27cc81b394199eb9
nathank33 <nathanbraunguitar@gmail.com>
2016-06-25 21:43:10 -0700

    Spouts now cannot be created above max height

--- src/com/projectkorra/projectkorra/airbending/AirSpout.java
+++ src/com/projectkorra/projectkorra/airbending/AirSpout.java
@@ -39,6 +39,11 @@ public class AirSpout extends AirAbility {
 		this.interval = getConfig().getLong("Abilities.Air.AirSpout.Interval");
 		this.height = getConfig().getDouble("Abilities.Air.AirSpout.Height");
 
+		double heightRemoveThreshold = 2;
+		if (!isWithinMaxSpoutHeight(heightRemoveThreshold)) {
+			return;
+		}
+		
 		new Flight(player);
 		start();
 		bPlayer.addCooldown(this);
@@ -69,6 +74,18 @@ public class AirSpout extends AirAbility {
 		player.setAllowFlight(true);
 		player.setFlying(true);
 	}
+	
+	private boolean isWithinMaxSpoutHeight(double threshold) {
+		Block ground = getGround();
+		if (ground == null) {
+			return false;
+		}
+		double playerHeight = player.getLocation().getY();
+		if (playerHeight > ground.getLocation().getY() + height + threshold) {
+			return false;
+		}
+		return true;
+	}
 
 	private Block getGround() {
 		Block standingblock = player.getLocation().getBlock();
@@ -84,6 +101,12 @@ public class AirSpout extends AirAbility {
 	@Override
 	public void progress() {
 		if (player.isDead() || !player.isOnline() || !bPlayer.canBendIgnoreBindsCooldowns(this)) {
+			remove();
+			return;
+		}
+		
+		double heightRemoveThreshold = 2;
+		if (!isWithinMaxSpoutHeight(heightRemoveThreshold)) {
 			remove();
 			return;
 		}
