2c076b50da59dd14ff6920b959ca5ced6dda1dd6
Archie <archie.irving@me.com>
2016-07-17 20:23:12 +0100

    EarthSmash & EarthBlast fixes (#516)
    * Adds slabs to default earth lists, stops slabs from changing to stone when fired
    
    * Fixes EarthSmash deleting sand / gravel blocks when riding EarthSmash into them

--- src/com/projectkorra/projectkorra/earthbending/EarthSmash.java
+++ src/com/projectkorra/projectkorra/earthbending/EarthSmash.java
@@ -399,7 +399,7 @@ public class EarthSmash extends EarthAbility {
 		for (BlockRepresenter blockRep : currentBlocks) {
 			Block block = location.clone().add(blockRep.getX(), blockRep.getY(), blockRep.getZ()).getBlock();
 			if (block.getType().equals(Material.SAND) || block.getType().equals(Material.GRAVEL)) { //Check if block can be affected by gravity.
-				addTempAirBlock(block); //If so, set it to a temp air block.
+
 			}
 			if (player != null && isTransparent(block)) {
 				affectedBlocks.add(new TempBlock(block, blockRep.getType(), blockRep.getData()));
