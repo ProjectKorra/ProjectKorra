13050a81cdea9818dce56f190d8c7c6775f8d8ff
Alexander Meech <Alex-The-Coder@users.noreply.github.com>
2016-10-18 20:49:54 -0400

    Update remote staff fetcher to translate... (#623)
    * Update remote staff fetcher to translate...
    
    ...color codes, use proper link, and use multiline entry management
    
    * Implement maven shading for external libraries and set up remote staff fetcher to appropriately handle html pages
    
    * Use maven instead of ant with travis

--- src/com/projectkorra/projectkorra/command/WhoCommand.java
+++ src/com/projectkorra/projectkorra/command/WhoCommand.java
@@ -1,15 +1,17 @@
 package com.projectkorra.projectkorra.command;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -17,6 +19,8 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import com.projectkorra.projectkorra.BendingPlayer;
 import com.projectkorra.projectkorra.Element;
@@ -52,29 +56,33 @@ public class WhoCommand extends PKCommand {
 		{
 			public void run()
 			{
-				try {
+				try
+				{
 					// Create a URL for the desired page
-					URL url = new URL("http://www.projectkorra.com/internals/staff.html");       
-
+					URLConnection url = new URL("http://www.projectkorra.com/staff.html").openConnection();
+					url.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+					
 					// Read all the text returned by the server
-					BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-					String unparsed = in.readLine(); // Read first line
-					if (unparsed != null)
+					BufferedReader in = new BufferedReader(new InputStreamReader(url.getInputStream(), Charset.forName("UTF-8")));
+					String unparsed;
+					while ((unparsed = in.readLine()) != null)
 					{
-						String[] staffUUIDS = unparsed.split("\\|");
-						for (String s : staffUUIDS)
+						Document doc = Jsoup.parse(unparsed);
+						String parsed = doc.body().text();
+						String[] staffEntry = parsed.split("/");
+						if (staffEntry.length >= 2)
 						{
-							String[] parts = s.split("/");
-							if (parts.length == 2)
-							{
-								staff.put(parts[0], parts[1]);
-							}
+							staff.put(staffEntry[0], ChatColor.translateAlternateColorCodes('&', staffEntry[1]));
 						}
 					}
 					in.close();
-				} catch (MalformedURLException e) {
+				}
+				catch (MalformedURLException e)
+				{
 					e.printStackTrace();
-				} catch (IOException e) {
+				}
+				catch (IOException e)
+				{
 					e.printStackTrace();
 				}
 			}
