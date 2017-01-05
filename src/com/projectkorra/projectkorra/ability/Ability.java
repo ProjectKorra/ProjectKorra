5c236c71858d067506815dcc462bb05805b88296
Archie Irving <archie.irving@me.com>
2017-01-05 03:39:27 +0000

    Adds getInstructions() method for abilities, rewrites descriptions of each ability
    - Adds getInstructions() method for each ability:
    Addon abilities will now have to add this method to explain how the
    move is activated, otherwise it will show ‘Usage: null’
    - Rewrites all descriptions of abilities and adds instructions:
    Ability description’s are now meant for general ability descriptions
    and the actually usage of the ability is in the instructions path.

--- src/com/projectkorra/projectkorra/ability/Ability.java
+++ src/com/projectkorra/projectkorra/ability/Ability.java
@@ -121,6 +121,16 @@ public interface Ability {
 	 * @return the most accurate Element that this ability belongs to
 	 * @see SubElement#getParentElement
 	 */
+	public String getInstructions();
+
+	/**
+	 * Creates a new instance of the combo from a specific player.
+	 * ProjectKorra's ComboModuleManager will use this method once the combo
+	 * steps have been used by the player.
+	 *
+	 * @return A new instance of the ability.
+	 * @param player The player using the combo.
+	 */
 	public Element getElement();
 
 	/**
