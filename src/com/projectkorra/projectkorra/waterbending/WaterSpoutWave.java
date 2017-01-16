d06f23693dcf752e53536d358b7ff81837ddee05
Christopher Martin <Christopherwmm@gmail.com>
2017-01-16 13:33:10 -0800

    Remove debug message left in WaterSpoutWave

--- src/com/projectkorra/projectkorra/waterbending/WaterSpoutWave.java
+++ src/com/projectkorra/projectkorra/waterbending/WaterSpoutWave.java
@@ -292,7 +292,6 @@ public class WaterSpoutWave extends WaterAbility {
 					for (Block block : FROZEN_BLOCKS.keySet()) {
 						TempBlock tBlock = FROZEN_BLOCKS.get(block);
 						if (tBlock.getBlock().getWorld().equals(player.getWorld()) && tBlock.getLocation().distance(player.getLocation()) >= thawRadius) {
-							Bukkit.broadcastMessage("revert");
 							tBlock.revertBlock();
 							FROZEN_BLOCKS.remove(block);
 						}
