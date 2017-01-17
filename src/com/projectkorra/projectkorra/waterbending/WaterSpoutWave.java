d3db7eaf74c637e1874cb4a76708c8f0ca1c9451
Sobki <Sobki@users.noreply.github.com>
2017-01-17 13:56:21 +1000

    Hot fix (#698)

--- src/com/projectkorra/projectkorra/waterbending/WaterSpoutWave.java
+++ src/com/projectkorra/projectkorra/waterbending/WaterSpoutWave.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
