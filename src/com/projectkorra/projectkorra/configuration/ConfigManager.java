a72e08bf161d3163a1b15a237754f6e4ed1d9482
Archie Irving <archie.irving@me.com>
2017-03-11 15:40:56 +0000

    Fixed rapid punch's damage
    https://trello.com/c/SqVLShgo/736-rapidpunch-isn-t-work-at-all

--- src/com/projectkorra/projectkorra/configuration/ConfigManager.java
+++ src/com/projectkorra/projectkorra/configuration/ConfigManager.java
@@ -1255,7 +1255,7 @@ public class ConfigManager {
 			config.addDefault("Abilities.Chi.Paralyze.Duration", 1500);
 
 			config.addDefault("Abilities.Chi.RapidPunch.Enabled", true);
-			config.addDefault("Abilities.Chi.RapidPunch.Damage", 1);
+			config.addDefault("Abilities.Chi.RapidPunch.Damage", 4);
 			config.addDefault("Abilities.Chi.RapidPunch.Distance", 3);
 			config.addDefault("Abilities.Chi.RapidPunch.Cooldown", 6000);
 			config.addDefault("Abilities.Chi.RapidPunch.Punches", 3);
