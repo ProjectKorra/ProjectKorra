b5932f3e5ce21129f797349bb92b9d261a6bab12
Archie Irving <archie.irving@me.com>
2017-06-13 01:02:46 +0100

    Fixes cooldowns for FireManipulation
    - Fixes Shield cooldown not activating if you stop using the move.
    - Fixed Stream cooldown not activating if you stop using the move.

--- src/com/projectkorra/projectkorra/firebending/FireManipulation.java
+++ src/com/projectkorra/projectkorra/firebending/FireManipulation.java
@@ -98,6 +98,7 @@ public class FireManipulation extends FireAbility {
 
 		if (!firing && !charging) {
 			if (!player.isSneaking()) {
+				bPlayer.addCooldown(this, shieldCooldown);
 				remove();
 				return;
 			} else if (System.currentTimeMillis() - getStartTime() > maxDuration) {
@@ -154,6 +155,7 @@ public class FireManipulation extends FireAbility {
 			}
 			if (!streamSneaking) {
 				direction = streamSneakDirection;
+				bPlayer.addCooldown(this, streamCooldown);
 				if (System.currentTimeMillis() - streamRemoveTime > 1000) {
 					remove();
 					return;
