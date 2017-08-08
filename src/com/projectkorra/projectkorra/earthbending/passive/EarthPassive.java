533aef440da6991af9a1026c08c473b9ca8be190
Christopher Martin <Christopherwmm@gmail.com>
2017-08-08 00:34:08 -0700

    Fixed formatting in EarthPassive

--- src/com/projectkorra/projectkorra/earthbending/passive/EarthPassive.java
+++ src/com/projectkorra/projectkorra/earthbending/passive/EarthPassive.java
@@ -7,7 +7,6 @@ import com.projectkorra.projectkorra.util.TempBlock;
 import org.bukkit.block.Block;
 
 public class EarthPassive {
-	
 	public static boolean canPhysicsChange(Block block) {
 		if (LavaSurgeWall.getAffectedBlocks().containsKey(block)) {
 			return false;
@@ -18,6 +17,7 @@ public class EarthPassive {
 		} else if (TempBlock.isTempBlock(block)) {
 			return false;
 		}
+		
 		return true;
 	}
 
@@ -29,37 +29,7 @@ public class EarthPassive {
 		} else if (LavaSurgeWave.isBlockWave(to) || LavaSurgeWave.isBlockWave(from)) {
 			return false;
 		}
+		
 		return true;
 	}
-	
-	/*@SuppressWarnings("deprecation")
-	public static void handleMetalPassives() {
-		for (Player player : Bukkit.getOnlinePlayers()) {
-			BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
-			if (bPlayer != null && bPlayer.canBendPassive(Element.EARTH) && bPlayer.canMetalbend()) {
-				if (player.isSneaking() && !bPlayer.isOnCooldown("MetalPassive")) {
-					Block block = player.getTargetBlock((HashSet<Material>) null, 5);
-					if (block == null) {
-						continue;
-					}
-					if (block.getType() == Material.IRON_DOOR_BLOCK && !GeneralMethods.isRegionProtectedFromBuild(player, block.getLocation())) {
-						if (block.getData() >= 8) {
-							block = block.getRelative(BlockFace.DOWN);
-						}
-						if (block.getData() < 4) {
-							block.setData((byte) (block.getData() + 4));
-							block.getWorld().playSound(block.getLocation(), Sound.BLOCK_IRON_DOOR_CLOSE, 10, 1);
-						} else {
-							block.setData((byte) (block.getData() - 4));
-							block.getWorld().playSound(block.getLocation(), Sound.BLOCK_IRON_DOOR_OPEN, 10, 1);
-						}
-						bPlayer.addCooldown("MetalPassive", 200);
-					}
-				}
-			}
-		}
-	}*/
-	
-
-
 }
