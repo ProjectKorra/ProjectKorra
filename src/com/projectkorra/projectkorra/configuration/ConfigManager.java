1c15f9209e0f5f3402dae9f06f0b53f0439553a2
PhanaticD <javw.phanatic@gmail.com>
2019-01-20 05:30:52 -0500

    add dynamic cooldown feature for airshield, off by default

--- src/com/projectkorra/projectkorra/configuration/ConfigManager.java
+++ src/com/projectkorra/projectkorra/configuration/ConfigManager.java
@@ -833,6 +833,7 @@ public class ConfigManager {
 			config.addDefault("Abilities.Air.AirShield.Streams", 5);
 			config.addDefault("Abilities.Air.AirShield.Speed", 10);
 			config.addDefault("Abilities.Air.AirShield.Particles", 5);
+			config.addDefault("Abilities.Air.AirShield.DynamicCooldown", false);
 
 			config.addDefault("Abilities.Air.AirSpout.Enabled", true);
 			config.addDefault("Abilities.Air.AirSpout.Cooldown", 0);
