e1c977ca7db9980100dea25304db7c0d174c2b54
Josh <javw.phanatic@gmail.com>
2019-07-03 13:08:41 -0400

    Catch any exception for custom worldguard flag register (#993)
    ## Misc. Changes
    * Changes exception for WorldGuard bending flag
    * Skip jukeboxes for TempBlocks to prevent invalid record type when trying to revert them

--- src/com/projectkorra/projectkorra/hooks/WorldGuardFlag.java
+++ src/com/projectkorra/projectkorra/hooks/WorldGuardFlag.java
@@ -2,7 +2,6 @@ package com.projectkorra.projectkorra.hooks;
 
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.flags.StateFlag;
-import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
 import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
 
 import com.projectkorra.projectkorra.ProjectKorra;
@@ -12,8 +11,8 @@ public class WorldGuardFlag {
 		final FlagRegistry registry = WorldGuard.getInstance().getFlagRegistry();
 		try {
 			registry.register(new StateFlag("bending", false));
-		} catch (final FlagConflictException e) {
-			ProjectKorra.log.severe("unable to register bending WorldGuard Flag");
+		} catch (final Exception e) {
+			ProjectKorra.log.severe("Unable to register bending WorldGuard Flag");
 		}
 	}
 }
