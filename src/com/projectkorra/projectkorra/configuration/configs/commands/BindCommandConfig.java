0e9838eb69d7cb1f8e34bfb68ab74164739b4441
Vahagn Tovmasian <vahagntovmasian@gmail.com>
2019-10-21 20:25:31 -0700

    Update BindCommandConfig.java

--- src/com/projectkorra/projectkorra/configuration/configs/commands/BindCommandConfig.java
+++ src/com/projectkorra/projectkorra/configuration/configs/commands/BindCommandConfig.java
@@ -2,16 +2,16 @@ package com.projectkorra.projectkorra.configuration.configs.commands;
 
 public class BindCommandConfig extends CommandConfig {
 
-	public final String SuccessfullyBoundMessage = "";
-	public final String AbilityDoesntExistMessage = "";
-	public final String WrongNumberMessage = "";
+	public final String SuccessfullyBoundMessage = "Succesfully bound {ability} to slot {slot}.";
+	public final String AbilityDoesntExistMessage = "{ability} is not a valid ability.";
+	public final String WrongNumberMessage = "Slot must be an integer between 1 and 9.";
 	public final String LoadingInfoMessage = "";
-	public final String ElementToggledOffMessage = "";
-	public final String NoElementMessage = "";
-	public final String NoElementMessageVowel = "";
-	public final String NoSubElementMessage = "";
-	public final String NoSubElementMessageVowel = "";
-	public final String UnbindableMessage = "";
+	public final String ElementToggledOffMessage = "You have that ability's element toggled off currently.";
+	public final String NoElementMessage = "You are not a {element}!";
+	public final String NoElementMessageVowel = "You are not an {element}!";
+	public final String NoSubElementMessage = "You don't have access to {subelement}!";
+	public final String NoSubElementMessageVowel = "You don't have access to {subelement}!";
+	public final String UnbindableMessage = "{ability} cannot be bound!";
 	
 	public BindCommandConfig() {
 		super("");
