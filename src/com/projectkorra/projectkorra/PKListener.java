38a804769b233a96e4136ae23de7fc09057b4cb0
Aztl <59033980+Aztlon@users.noreply.github.com>
2020-08-05 14:49:09 -0400

    Changed WaterArms displayBoundMsg condition
    Under onPlayerSlotChange, it would send the player messages with info on their subabilities if BendingPreview was set to false. Now, the condition has changed, so that if "DisplayBoundMsg" (a new config option under WaterArms) is set to true, it would do the same thing. This allows for more configurability instead of tying this to another config option. Sometimes people want BendingPreview off but don't want to see the WaterArms messages.

--- src/com/projectkorra/projectkorra/PKListener.java
+++ src/com/projectkorra/projectkorra/PKListener.java
@@ -1585,7 +1585,7 @@ public class PKListener implements Listener {
 		final int slot = event.getNewSlot() + 1;
 		GeneralMethods.displayMovePreview(player, slot);
 
-		if (!ConfigManager.defaultConfig.get().getBoolean("Properties.BendingPreview")) {
+		if (ConfigManager.defaultConfig.get().getBoolean("Abilities.Water.WaterArms.DisplayBoundMsg")) {
 			final WaterArms waterArms = CoreAbility.getAbility(player, WaterArms.class);
 			if (waterArms != null) {
 				waterArms.displayBoundMsg(event.getNewSlot() + 1);
