11d9fa16c235e4a9d23ae1901361d232cf3c3461
Aztl <59033980+Aztlon@users.noreply.github.com>
2020-08-05 14:38:33 -0400

    Added a configurable hit radius for AirSweep

--- src/com/projectkorra/projectkorra/airbending/combo/AirSweep.java
+++ src/com/projectkorra/projectkorra/airbending/combo/AirSweep.java
@@ -42,6 +42,7 @@ public class AirSweep extends AirAbility implements ComboAbility {
 	private Vector direction;
 	private ArrayList<Entity> affectedEntities;
 	private ArrayList<BukkitRunnable> tasks;
+	private double hitRadius;
 
 	public AirSweep(final Player player) {
 		super(player);
@@ -62,6 +63,7 @@ public class AirSweep extends AirAbility implements ComboAbility {
 		this.speed = getConfig().getDouble("Abilities.Air.AirSweep.Speed");
 		this.knockback = getConfig().getDouble("Abilities.Air.AirSweep.Knockback");
 		this.cooldown = getConfig().getLong("Abilities.Air.AirSweep.Cooldown");
+		this.hitRadius = getConfig().getDouble("Abilities.Air.AirSweep.HitRadius");
 
 		if (this.bPlayer.isAvatarState()) {
 			this.cooldown = 0;
@@ -188,7 +190,7 @@ public class AirSweep extends AirAbility implements ComboAbility {
 				}
 			}
 			if (i % 3 == 0) {
-				for (final Entity entity : GeneralMethods.getEntitiesAroundPoint(loc, 2.5)) {
+				for (final Entity entity : GeneralMethods.getEntitiesAroundPoint(loc, hitRadius)) {
 					if (GeneralMethods.isRegionProtectedFromBuild(this, entity.getLocation())) {
 						this.remove();
 						return;
