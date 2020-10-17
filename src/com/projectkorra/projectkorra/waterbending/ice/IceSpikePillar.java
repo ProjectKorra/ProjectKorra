f7870c5484560225dff6edaf5a3b98d9c2db35fd
dniym <sales@whoolie.com>
2020-10-17 11:26:52 -0400

    Update IceSpikePillar.java

--- src/com/projectkorra/projectkorra/waterbending/ice/IceSpikePillar.java
+++ src/com/projectkorra/projectkorra/waterbending/ice/IceSpikePillar.java
@@ -17,7 +17,6 @@ import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
 import com.projectkorra.projectkorra.GeneralMethods;
-import com.projectkorra.projectkorra.ability.Ability;
 import com.projectkorra.projectkorra.ability.AirAbility;
 import com.projectkorra.projectkorra.ability.ElementalAbility;
 import com.projectkorra.projectkorra.ability.IceAbility;
