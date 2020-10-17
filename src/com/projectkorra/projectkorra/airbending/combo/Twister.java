b73f330ab8c7a2798cdd0c84b33fe8d43e8cf45b
dniym <sales@whoolie.com>
2020-10-17 11:08:23 -0400

    Update Twister.java

--- src/com/projectkorra/projectkorra/airbending/combo/Twister.java
+++ src/com/projectkorra/projectkorra/airbending/combo/Twister.java
@@ -9,7 +9,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 import com.projectkorra.projectkorra.GeneralMethods;
-import com.projectkorra.projectkorra.ability.Ability;
 import com.projectkorra.projectkorra.ability.AirAbility;
 import com.projectkorra.projectkorra.ability.ComboAbility;
 import com.projectkorra.projectkorra.ability.util.ComboManager.AbilityInformation;
@@ -147,7 +146,7 @@ public class Twister extends AirAbility implements ComboAbility {
 				continue;
 			}
 			final Vector forceDir = GeneralMethods.getDirection(entity.getLocation(), this.currentLoc.clone().add(0, height, 0));
-			GeneralMethods.setVelocity(this,entity, forceDir.clone().normalize().multiply(0.3));
+			GeneralMethods.setVelocity(this, entity, forceDir.clone().normalize().multiply(0.3));
 			
 		}
 	}
