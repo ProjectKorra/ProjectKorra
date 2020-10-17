9b0708a0dedd58f01b93ba7898d7fbdc99303044
dniym <sales@whoolie.com>
2020-10-17 11:27:12 -0400

    Update WaterArmsWhip.java

--- src/com/projectkorra/projectkorra/waterbending/multiabilities/WaterArmsWhip.java
+++ src/com/projectkorra/projectkorra/waterbending/multiabilities/WaterArmsWhip.java
@@ -13,7 +13,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 import com.projectkorra.projectkorra.GeneralMethods;
-import com.projectkorra.projectkorra.ability.Ability;
 import com.projectkorra.projectkorra.ability.WaterAbility;
 import com.projectkorra.projectkorra.ability.util.MultiAbilityManager;
 import com.projectkorra.projectkorra.attribute.Attribute;
