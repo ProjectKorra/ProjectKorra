d247162afcac1b078397c86c46a77efaedcc1a25
Robbie Gorey <59033980+Aztlon@users.noreply.github.com>
2021-06-03 17:46:36 -0400

    Remove unnecessary parameters
    wip

--- src/com/projectkorra/projectkorra/event/SimulatedBlockPlaceEvent.java
+++ src/com/projectkorra/projectkorra/event/SimulatedBlockPlaceEvent.java
@@ -1,11 +1,10 @@
 package com.projectkorra.projectkorra.event;
 
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.inventory.ItemStack;
 
 /**
  * Called as a test event for checking if other plugins cancel the block placing
@@ -13,8 +12,8 @@ import org.bukkit.inventory.ItemStack;
  */
 public class SimulatedBlockPlaceEvent extends BlockPlaceEvent {
 
-	public SimulatedBlockPlaceEvent(Block placedBlock, BlockState replacedBlockState, Block placedAgainst, ItemStack itemInHand, Player thePlayer, boolean canBuild, EquipmentSlot hand) {
-		super(placedBlock, replacedBlockState, placedAgainst, itemInHand, thePlayer, canBuild, hand);
+	public SimulatedBlockPlaceEvent(Block block, Player player) {
+		super(block, block.getState(), block.getRelative(BlockFace.DOWN), player.getInventory().getItemInMainHand(), player, true, EquipmentSlot.HAND);
 	}
 
 }
