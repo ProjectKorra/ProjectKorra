9967870607f004c971b4a98fa896b05ac1d4d0a0
Hernán Indíbil de la Cruz Calvo <indikalesken@hotmail.com>
2021-06-03 22:02:36 +0200

    Removed import that I added accidentally

--- src/com/projectkorra/projectkorra/storage/SQLite.java
+++ src/com/projectkorra/projectkorra/storage/SQLite.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 import java.util.logging.Logger;
 
 public class SQLite extends Database {
