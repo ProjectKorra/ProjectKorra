10b4b114ba31c0a3a5304225ff93de06b9355433
Robbie Gorey <59033980+Aztlon@users.noreply.github.com>
2021-06-03 18:57:16 -0400

    Make the item static and add imports

--- src/com/projectkorra/projectkorra/event/SimulatedBlockPlaceEvent.java
+++ src/com/projectkorra/projectkorra/event/SimulatedBlockPlaceEvent.java
@@ -1,19 +1,23 @@
 package com.projectkorra.projectkorra.event;
 
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Called as a test event for checking if other plugins cancel the block placing
  * @see {@link com.projectkorra.projectkorra.GeneralMethods#isRegionProtectedFromBuildPostCache}
  */
 public class SimulatedBlockPlaceEvent extends BlockPlaceEvent {
+	
+	private static final ItemStack ITEM_KEY = new ItemStack(Material.PETRIFIED_OAK_SLAB);
 
 	public SimulatedBlockPlaceEvent(Block block, Player player) {
-		super(block, block.getState(), block.getRelative(BlockFace.DOWN), new ItemStack(Material.PETRIFIED_OAK_SLAB), player, true, EquipmentSlot.HAND);
+		super(block, block.getState(), block.getRelative(BlockFace.DOWN), ITEM_KEY, player, true, EquipmentSlot.HAND);
 	}
 
 }
