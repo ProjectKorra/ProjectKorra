761569bbcf740ee0698ab31710370e16cfc8981b
Christopher Martin <Christopherwmm@gmail.com>
2021-07-17 09:58:36 -0700

    Change RevertCheckTime to match the default

--- src/com/projectkorra/projectkorra/configuration/ConfigManager.java
+++ src/com/projectkorra/projectkorra/configuration/ConfigManager.java
@@ -1243,7 +1243,7 @@ public class ConfigManager {
 			config.addDefault("Abilities.Earth.EarthGrab.DamageThreshold", 4);
 
 			config.addDefault("Abilities.Earth.EarthTunnel.Enabled", true);
-			config.addDefault("Abilities.Earth.EarthTunnel.RevertCheckTime", 30000);
+			config.addDefault("Abilities.Earth.EarthTunnel.RevertCheckTime", 300000);
 			config.addDefault("Abilities.Earth.EarthTunnel.Cooldown", 0);
 			config.addDefault("Abilities.Earth.EarthTunnel.MaxRadius", 1);
 			config.addDefault("Abilities.Earth.EarthTunnel.Range", 10);
