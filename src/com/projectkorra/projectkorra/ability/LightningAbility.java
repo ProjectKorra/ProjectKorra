e7b630c3c96a52496b7fd9dd120de445c9d8cf81
Roman Shamov <dreigfire@mail.ru>
2022-10-10 08:20:02 +0300

    - Added functional particles to LightningAbility and PlantAbility

--- src/com/projectkorra/projectkorra/ability/LightningAbility.java
+++ src/com/projectkorra/projectkorra/ability/LightningAbility.java
@@ -2,6 +2,7 @@ package com.projectkorra.projectkorra.ability;
 
 import com.projectkorra.projectkorra.GeneralMethods;
 import com.projectkorra.projectkorra.ProjectKorra;
+import com.projectkorra.projectkorra.ability.functional.Functional;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -10,113 +11,117 @@ import com.projectkorra.projectkorra.Element;
 
 public abstract class LightningAbility extends FireAbility implements SubAbility {
 
-	public LightningAbility(final Player player) {
-		super(player);
-	}
+    public LightningAbility(final Player player) {
+        super(player);
+    }
 
-	public static void playLightningbendingHitSound(final Location loc) {
-		if (getConfig().getBoolean("Properties.Fire.PlaySound")) {
-			final float volume = (float) getConfig().getDouble("Properties.Fire.LightningHit.Volume");
-			final float pitch = (float) getConfig().getDouble("Properties.Fire.LightningHit.Pitch");
+    public static void playLightningbendingHitSound(final Location loc) {
+        if (getConfig().getBoolean("Properties.Fire.PlaySound")) {
+            final float volume = (float) getConfig().getDouble("Properties.Fire.LightningHit.Volume");
+            final float pitch = (float) getConfig().getDouble("Properties.Fire.LightningHit.Pitch");
 
-			Sound sound = Sound.ENTITY_LIGHTNING_BOLT_THUNDER;
-			try {
-				sound = Sound.valueOf(getConfig().getString("Properties.Fire.LightningHit.Sound"));
-			} catch (final IllegalArgumentException exception) {
-				ProjectKorra.log.warning("Your current value for 'Properties.Fire.LightningHit.Sound' is not valid.");
-			} finally {
-				loc.getWorld().playSound(loc, sound, volume, pitch);
-			}
-		}
-	}
+            Sound sound = Sound.ENTITY_LIGHTNING_BOLT_THUNDER;
+            try {
+                sound = Sound.valueOf(getConfig().getString("Properties.Fire.LightningHit.Sound"));
+            } catch (final IllegalArgumentException exception) {
+                ProjectKorra.log.warning("Your current value for 'Properties.Fire.LightningHit.Sound' is not valid.");
+            } finally {
+                loc.getWorld().playSound(loc, sound, volume, pitch);
+            }
+        }
+    }
 
-	public static void playLightningbendingChargingSound(final Location loc) {
-		if (getConfig().getBoolean("Properties.Fire.PlaySound")) {
-			final float volume = (float) getConfig().getDouble("Properties.Fire.LightningCharge.Volume");
-			final float pitch = (float) getConfig().getDouble("Properties.Fire.LightningCharge.Pitch");
+    public static void playLightningbendingChargingSound(final Location loc) {
+        if (getConfig().getBoolean("Properties.Fire.PlaySound")) {
+            final float volume = (float) getConfig().getDouble("Properties.Fire.LightningCharge.Volume");
+            final float pitch = (float) getConfig().getDouble("Properties.Fire.LightningCharge.Pitch");
 
-			Sound sound = Sound.BLOCK_BEEHIVE_WORK;
-			try {
-				sound = Sound.valueOf(getConfig().getString("Properties.Fire.LightningCharge.Sound"));
-			} catch (final IllegalArgumentException exception) {
-				ProjectKorra.log.warning("Your current value for 'Properties.Fire.LightningCharge.Sound' is not valid.");
-			} finally {
-				loc.getWorld().playSound(loc, sound, volume, pitch);
-			}
-		}
-	}
+            Sound sound = Sound.BLOCK_BEEHIVE_WORK;
+            try {
+                sound = Sound.valueOf(getConfig().getString("Properties.Fire.LightningCharge.Sound"));
+            } catch (final IllegalArgumentException exception) {
+                ProjectKorra.log.warning("Your current value for 'Properties.Fire.LightningCharge.Sound' is not valid.");
+            } finally {
+                loc.getWorld().playSound(loc, sound, volume, pitch);
+            }
+        }
+    }
 
-	public static void playLightningbendingSound(final Location loc) {
-		if (getConfig().getBoolean("Properties.Fire.PlaySound")) {
-			final float volume = (float) getConfig().getDouble("Properties.Fire.LightningSound.Volume");
-			final float pitch = (float) getConfig().getDouble("Properties.Fire.LightningSound.Pitch");
+    public static void playLightningbendingSound(final Location loc) {
+        if (getConfig().getBoolean("Properties.Fire.PlaySound")) {
+            final float volume = (float) getConfig().getDouble("Properties.Fire.LightningSound.Volume");
+            final float pitch = (float) getConfig().getDouble("Properties.Fire.LightningSound.Pitch");
 
-			Sound sound = Sound.ENTITY_CREEPER_HURT;
-			try {
-				sound = Sound.valueOf(getConfig().getString("Properties.Fire.LightningSound.Sound"));
-			} catch (final IllegalArgumentException exception) {
-				ProjectKorra.log.warning("Your current value for 'Properties.Fire.LightningSound.Sound' is not valid.");
-			} finally {
-				loc.getWorld().playSound(loc, sound, volume, pitch);
-			}
-		}
-	}
+            Sound sound = Sound.ENTITY_CREEPER_HURT;
+            try {
+                sound = Sound.valueOf(getConfig().getString("Properties.Fire.LightningSound.Sound"));
+            } catch (final IllegalArgumentException exception) {
+                ProjectKorra.log.warning("Your current value for 'Properties.Fire.LightningSound.Sound' is not valid.");
+            } finally {
+                loc.getWorld().playSound(loc, sound, volume, pitch);
+            }
+        }
+    }
 
-	/**
-	 * Plays an integer amount of lightning particles in a location with a given
-	 * xOffset, yOffset, and zOffset.
-	 *
-	 * @param ability The ability this particle is spawned for
-	 * @param loc     The location to use
-	 * @param amount  The amount of particles
-	 * @param xOffset The xOffset to use
-	 * @param yOffset The yOffset to use
-	 * @param zOffset The zOffset to use
-	 */
-	public static void playLightningbendingParticles(final CoreAbility ability, final Location loc, final int amount, final double xOffset, final double yOffset, final double zOffset) {
-		GeneralMethods.displayColoredParticle("#01E1FF", loc, amount, xOffset, yOffset, zOffset);
-	}
+    public static Functional.Particle lightningParticles = (ability, location, amount, xOffset, yOffset, zOffset, extra, data) -> {
+        GeneralMethods.displayColoredParticle("#01E1FF", location, amount, xOffset, yOffset, zOffset);
+    };
 
-	/**
-	 * Plays an integer amount of lightning particles in a location.
-	 *
-	 * @param ability The ability this particle is spawned for
-	 * @param loc     The location to use
-	 * @param amount  The amount of particles
-	 */
-	public static void playLightningbendingParticles(final CoreAbility ability, final Location loc, final int amount) {
-		playLightningbendingParticles(ability, loc, amount, Math.random(), Math.random(), Math.random());
-	}
+    /**
+     * Plays an integer amount of lightning particles in a location with a given
+     * xOffset, yOffset, and zOffset.
+     *
+     * @param ability The ability this particle is spawned for
+     * @param loc     The location to use
+     * @param amount  The amount of particles
+     * @param xOffset The xOffset to use
+     * @param yOffset The yOffset to use
+     * @param zOffset The zOffset to use
+     */
+    public static void playLightningbendingParticles(final CoreAbility ability, final Location loc, final int amount, final double xOffset, final double yOffset, final double zOffset) {
+        lightningParticles.play(ability, loc, amount, xOffset, yOffset, zOffset, 0, null);
+    }
 
-	@Override
-	public Class<? extends Ability> getParentAbility() {
-		return FireAbility.class;
-	}
+    /**
+     * Plays an integer amount of lightning particles in a location.
+     *
+     * @param ability The ability this particle is spawned for
+     * @param loc     The location to use
+     * @param amount  The amount of particles
+     */
+    public static void playLightningbendingParticles(final CoreAbility ability, final Location loc, final int amount) {
+        playLightningbendingParticles(ability, loc, amount, Math.random(), Math.random(), Math.random());
+    }
 
-	@Override
-	public Element getElement() {
-		return Element.LIGHTNING;
-	}
+    @Override
+    public Class<? extends Ability> getParentAbility() {
+        return FireAbility.class;
+    }
 
-	@Override
-	public boolean isIgniteAbility() {
-		return false;
-	}
+    @Override
+    public Element getElement() {
+        return Element.LIGHTNING;
+    }
 
-	//Overriding these methods to make sure Lightning abilities don't get buffed by blue fire
-	@Override
-	public double applyModifiersDamage(double value) {
-		return GeneralMethods.applyModifiers(value, getDayFactor(1.0));
-	}
+    @Override
+    public boolean isIgniteAbility() {
+        return false;
+    }
 
-	@Override
-	public double applyModifiersRange(double value) {
-		return GeneralMethods.applyModifiers(value, getDayFactor(1.0));
-	}
+    //Overriding these methods to make sure Lightning abilities don't get buffed by blue fire
+    @Override
+    public double applyModifiersDamage(double value) {
+        return GeneralMethods.applyModifiers(value, getDayFactor(1.0));
+    }
 
-	@Override
-	public long applyModifiersCooldown(long value) {
-		return (long) GeneralMethods.applyInverseModifiers(value, getDayFactor(1.0));
-	}
+    @Override
+    public double applyModifiersRange(double value) {
+        return GeneralMethods.applyModifiers(value, getDayFactor(1.0));
+    }
+
+    @Override
+    public long applyModifiersCooldown(long value) {
+        return (long) GeneralMethods.applyInverseModifiers(value, getDayFactor(1.0));
+    }
 
 }
--- src/com/projectkorra/projectkorra/ability/PlantAbility.java
+++ src/com/projectkorra/projectkorra/ability/PlantAbility.java
@@ -1,8 +1,11 @@
 package com.projectkorra.projectkorra.ability;
 
+import com.projectkorra.projectkorra.ProjectKorra;
+import com.projectkorra.projectkorra.ability.functional.Functional;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
 import com.projectkorra.projectkorra.Element;
@@ -12,7 +15,7 @@ public abstract class PlantAbility extends WaterAbility implements SubAbility {
 	public PlantAbility(final Player player) {
 		super(player);
 	}
-
+	
 	@Override
 	public Class<? extends Ability> getParentAbility() {
 		return WaterAbility.class;
@@ -23,9 +26,29 @@ public abstract class PlantAbility extends WaterAbility implements SubAbility {
 		return Element.PLANT;
 	}
 
+	public Functional.Particle plantParticles = (ability, location, amount, xOffset, yOffset, zOffset, extra, data) -> {
+		location.getWorld().spawnParticle(Particle.BLOCK_CRACK, location.clone().add(0.5, 0, 0.5), amount, xOffset, yOffset, zOffset, data);
+	};
+	
 	// Because Plantbending deserves particles too!
-	public void playPlantbendingParticles(final Location loc, final int amount, final double xOffset, final double yOffset, final double zOffset) {
-		loc.getWorld().spawnParticle(Particle.BLOCK_CRACK, loc.clone().add(0.5, 0, 0.5), amount, xOffset, yOffset, zOffset, Material.OAK_LEAVES.createBlockData());
+	public void playPlantbendingParticles(CoreAbility ability, final Location loc, final int amount, final double xOffset, final double yOffset, final double zOffset) {
+		plantParticles.play(ability, loc, amount, xOffset, yOffset, zOffset, 0, Material.OAK_LEAVES.createBlockData());
 	}
 
+	public static void playPlantbendingSound(final Location loc) {
+		if (getConfig().getBoolean("Properties.Water.PlaySound")) {
+			final float volume = (float) getConfig().getDouble("Properties.Water.PlantSound.Volume");
+			final float pitch = (float) getConfig().getDouble("Properties.Water.PlantSound.Pitch");
+
+			Sound sound = Sound.BLOCK_GRASS_STEP;
+
+			try {
+				sound = Sound.valueOf(getConfig().getString("Properties.Water.PlantSound.Sound"));
+			} catch (final IllegalArgumentException exception) {
+				ProjectKorra.log.warning("Your current value for 'Properties.Water.PlantSound.Sound' is not valid.");
+			} finally {
+				loc.getWorld().playSound(loc, sound, volume, pitch);
+			}
+		}
+	}
 }
