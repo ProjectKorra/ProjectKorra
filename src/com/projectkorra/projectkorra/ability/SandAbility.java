89e5143e3bda7ff71362c2fac5d9cbdf6e03c453
Roman Shamov <dreigfire@mail.ru>
2022-10-13 15:27:44 +0300

    - Added Functional Particle to SandAbility class.

--- src/com/projectkorra/projectkorra/ability/SandAbility.java
+++ src/com/projectkorra/projectkorra/ability/SandAbility.java
@@ -1,6 +1,7 @@
 package com.projectkorra.projectkorra.ability;
 
 import com.projectkorra.projectkorra.ProjectKorra;
+import com.projectkorra.projectkorra.ability.functional.Functional;
 import com.projectkorra.projectkorra.util.ParticleEffect;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -11,60 +12,60 @@ import com.projectkorra.projectkorra.Element;
 
 public abstract class SandAbility extends EarthAbility implements SubAbility {
 
-	public SandAbility(final Player player) {
-		super(player);
-	}
+    public SandAbility(final Player player) {
+        super(player);
+    }
 
-	/**
-	 * Plays an integer amount of sand particles in a location with a given
-	 * xOffset, yOffset, zOffset, speed and color.
-	 *
-	 * @param ability The ability this particle is spawned for
-	 * @param loc The location to use
-	 * @param amount The amount of particles
-	 * @param xOffset The xOffset to use
-	 * @param yOffset The yOffset to use
-	 * @param zOffset The zOffset to use
-	 * @param speed The particle animation speed
-	 * @param red Use red sand for the particle
-	 */
-	public static void displaySandParticle(final CoreAbility ability, final Location loc, final int amount, final double xOffset, final double yOffset, final double zOffset, final double speed, final boolean red) {
-		if (amount <= 0) {
-			return;
-		}
+    public static Functional.Particle sandParticles = (ability, location, amount, xOffset, yOffset, zOffset, extra, data) -> {
+        if (amount <= 0) {
+            return;
+        }
+        ParticleEffect.BLOCK_CRACK.display(location, amount, xOffset, yOffset, zOffset, extra, data);
+        ParticleEffect.FALLING_DUST.display(location, amount, xOffset, yOffset, zOffset, extra, data);
+    };
 
-		final Material sand = red ? Material.RED_SAND : Material.SAND;
-		final Material stone = red ? Material.RED_SANDSTONE : Material.SANDSTONE;
+    /**
+     * Plays an integer amount of sand particles in a location with a given
+     * xOffset, yOffset, zOffset, speed and color.
+     *
+     * @param ability The ability this particle is spawned for
+     * @param loc     The location to use
+     * @param amount  The amount of particles
+     * @param xOffset The xOffset to use
+     * @param yOffset The yOffset to use
+     * @param zOffset The zOffset to use
+     * @param speed   The particle animation speed
+     * @param red     Use red sand for the particle
+     */
+    public static void displaySandParticle(final CoreAbility ability, final Location loc, final int amount, final double xOffset, final double yOffset, final double zOffset, final double speed, final boolean red) {
+        sandParticles.play(ability, loc, amount, xOffset, yOffset, zOffset, speed, (red ? Material.RED_SAND : Material.SAND).createBlockData());
+    }
 
-		ParticleEffect.BLOCK_CRACK.display(loc, amount, xOffset, yOffset, zOffset, speed, sand.createBlockData());
-		ParticleEffect.BLOCK_CRACK.display(loc, amount, xOffset, yOffset, zOffset, speed, stone.createBlockData());
-	}
+    public static void playSandbendingSound(final Location loc) {
+        if (getConfig().getBoolean("Properties.Earth.PlaySound")) {
+            final float volume = (float) getConfig().getDouble("Properties.Earth.SandSound.Volume");
+            final float pitch = (float) getConfig().getDouble("Properties.Earth.SandSound.Pitch");
 
-	public static void playSandbendingSound(final Location loc) {
-		if (getConfig().getBoolean("Properties.Earth.PlaySound")) {
-			final float volume = (float) getConfig().getDouble("Properties.Earth.SandSound.Volume");
-			final float pitch = (float) getConfig().getDouble("Properties.Earth.SandSound.Pitch");
+            Sound sound = Sound.BLOCK_SAND_BREAK;
 
-			Sound sound = Sound.BLOCK_SAND_BREAK;
+            try {
+                sound = Sound.valueOf(getConfig().getString("Properties.Earth.SandSound.Sound"));
+            } catch (final IllegalArgumentException exception) {
+                ProjectKorra.log.warning("Your current value for 'Properties.Earth.SandSound.Sound' is not valid.");
+            } finally {
+                loc.getWorld().playSound(loc, sound, volume, pitch);
+            }
+        }
+    }
 
-			try {
-				sound = Sound.valueOf(getConfig().getString("Properties.Earth.SandSound.Sound"));
-			} catch (final IllegalArgumentException exception) {
-				ProjectKorra.log.warning("Your current value for 'Properties.Earth.SandSound.Sound' is not valid.");
-			} finally {
-				loc.getWorld().playSound(loc, sound, volume, pitch);
-			}
-		}
-	}
+    @Override
+    public Class<? extends Ability> getParentAbility() {
+        return EarthAbility.class;
+    }
 
-	@Override
-	public Class<? extends Ability> getParentAbility() {
-		return EarthAbility.class;
-	}
-
-	@Override
-	public Element getElement() {
-		return Element.SAND;
-	}
+    @Override
+    public Element getElement() {
+        return Element.SAND;
+    }
 
 }
