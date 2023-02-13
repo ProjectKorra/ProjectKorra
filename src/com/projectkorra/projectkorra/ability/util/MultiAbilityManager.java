e5f96e2a0da4513fb7cb5a45603f93b7df0ff243
StrangeOne101 <strange.toby@gmail.com>
2023-02-13 22:10:43 +1300

    Fixed relogging with a Multi-bind causing binds to clear

--- src/com/projectkorra/projectkorra/ability/util/MultiAbilityManager.java
+++ src/com/projectkorra/projectkorra/ability/util/MultiAbilityManager.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.projectkorra.projectkorra.OfflineBendingPlayer;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 import com.projectkorra.projectkorra.BendingPlayer;
@@ -111,10 +113,7 @@ public class MultiAbilityManager {
 	 * @return true If player has a multiability active
 	 */
 	public static boolean hasMultiAbilityBound(final Player player) {
-		if (playerAbilities.containsKey(player)) {
-			return true;
-		}
-		return false;
+		return playerAbilities.containsKey(player);
 	}
 
 	/**
@@ -141,15 +140,19 @@ public class MultiAbilityManager {
 	 * @param player
 	 */
 	public static void remove(final Player player) {
-		playerAbilities.remove(player);
-		playerBoundAbility.remove(player);
-		playerSlot.remove(player);
+		if (playerAbilities.containsKey(player)) {
+			unbindMultiAbility(player);
+		}
 	}
 
 	/**
 	 * Cleans up all MultiAbilities.
 	 */
 	public static void removeAll() {
+		for (Player player : playerAbilities.keySet()) {
+			unbindMultiAbility(player);
+		}
+
 		playerAbilities.clear();
 		playerSlot.clear();
 		playerBoundAbility.clear();
@@ -178,31 +181,29 @@ public class MultiAbilityManager {
 	 * @param player
 	 */
 	public static void unbindMultiAbility(final Player player) {
-		if (!player.isOnline()) {
-			return;
-		}
-		
 		playerAbilities.compute(player, MultiAbilityManager::resetBinds);
 		playerBoundAbility.remove(player);
 		playerSlot.remove(player);
 	}
 	
-	private static HashMap<Integer, String> resetBinds(Player player, HashMap<Integer, String> prevBinds) {
+	private static HashMap<Integer, String> resetBinds(OfflinePlayer player, HashMap<Integer, String> prevBinds) {
 		if (prevBinds == null) {
 			return null;
 		}
 		
-		final BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+		final OfflineBendingPlayer bPlayer = BendingPlayer.getOrLoadOffline(player);
 		if (bPlayer == null) {
 			return null;
 		}
-		
-		player.getInventory().setHeldItemSlot(playerSlot.getOrDefault(player, 0));
-		ProjectKorra.plugin.getServer().getPluginManager().callEvent(new PlayerBindChangeEvent(player, playerBoundAbility.get(player), false, true));
 
-		for (int i = 1; i < 10; i++) {
-			bPlayer.getAbilities().put(i, prevBinds.get(i));
+
+		if (player instanceof Player) {
+			((Player)player).getInventory().setHeldItemSlot(playerSlot.getOrDefault(player, 0));
+			ProjectKorra.plugin.getServer().getPluginManager().callEvent(new PlayerBindChangeEvent((Player) player, playerBoundAbility.get(player), false, true));
 		}
+
+		bPlayer.getAbilities().clear();
+		bPlayer.getAbilities().putAll(prevBinds);
 		
 		return null;
 	}
