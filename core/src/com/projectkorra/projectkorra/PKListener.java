cd3019df1251a76d4dd3fb8f99be9b46287b28e6
Manu585 <manuelgogg2@gmail.com>
2025-06-16 09:10:17 +0200

    Made Config-based enabled check and also checking if player has permission for ability before trying to instantiate EarthPillars

--- core/src/com/projectkorra/projectkorra/PKListener.java
+++ core/src/com/projectkorra/projectkorra/PKListener.java
@@ -943,8 +943,10 @@ public class PKListener implements Listener {
 				if (bPlayer.getBoundAbilityName().equalsIgnoreCase("Shockwave")) {
 					new Shockwave(player, true);
 				} else if (bPlayer.getBoundAbilityName().equalsIgnoreCase("Catapult")) {
-					if (CoreAbility.getAbility(EarthPillars.class).isEnabled()) {
-						new EarthPillars(player, true);
+					if (ConfigManager.defaultConfig.get().getBoolean("Abilities.Earth.EarthPillars.Enabled")) {
+						if (bPlayer.canBend(CoreAbility.getAbility(EarthPillars.class))) {
+							new EarthPillars(player, true);
+						}
 					}
 				}
 			}
