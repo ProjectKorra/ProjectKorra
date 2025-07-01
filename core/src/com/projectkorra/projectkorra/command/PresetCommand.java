7272262d846125e95798bf322b14d886e2a4bfff
Manu585 <manuelgogg2@gmail.com>
2025-07-01 10:43:49 +0200

    Added base perm check; Removed redundant Language Config option

--- core/src/com/projectkorra/projectkorra/command/PresetCommand.java
+++ core/src/com/projectkorra/projectkorra/command/PresetCommand.java
@@ -389,7 +389,7 @@ public class PresetCommand extends PKCommand {
 			return;
 		}
 
-		if (newName == null || newName.isBlank()) {
+		if (newName == null || newName.isBlank() || newName == oldName) {
 			oldPreset.update(new HashMap<>(BendingPlayer.getBendingPlayer(player).getAbilities())).thenAccept(success -> {
 				if (success) {
 					ChatUtil.sendBrandingMessage(player, ChatColor.GREEN + updatedPresetMessage.replace("{name}", ChatColor.YELLOW + oldName + ChatColor.GREEN));
