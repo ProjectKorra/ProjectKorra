8e69cbeee59ff6cd7aaaeae92aeb40e2ac752b6d
kiwisshouldfly <kikibird32@gmail.com>
2025-07-17 10:53:07 +1200

    Changed Firewheel Hitbox
    Moved over to 4n branch (hopefully successfully)
    
    - added to config for easy access

--- core/src/com/projectkorra/projectkorra/firebending/combo/FireWheel.java
+++ core/src/com/projectkorra/projectkorra/firebending/combo/FireWheel.java
@@ -1,7 +1,6 @@
 package com.projectkorra.projectkorra.firebending.combo;
 
 import java.util.ArrayList;
-
 import com.projectkorra.projectkorra.ability.util.ComboUtil;
 import com.projectkorra.projectkorra.attribute.markers.DayNightFactor;
 import com.projectkorra.projectkorra.configuration.ConfigManager;
@@ -36,6 +35,7 @@ public class FireWheel extends FireAbility implements ComboAbility {
 	@Attribute(Attribute.HEIGHT) @DayNightFactor
 	private double height;
 	private double radius;
+	private double hitboxmultiplier;
 	@Attribute(Attribute.SPEED) @DayNightFactor
 	private double speed;
 	@Attribute(Attribute.FIRE_TICK) @DayNightFactor
@@ -54,6 +54,7 @@ public class FireWheel extends FireAbility implements ComboAbility {
 
 		this.damage = getConfig().getDouble("Abilities.Fire.FireWheel.Damage");
 		this.range = getConfig().getDouble("Abilities.Fire.FireWheel.Range");
+		this.hitboxmultiplier = getConfig().getDouble("Abilities.Fire.FireWheel.HitboxMultiplier");
 		this.speed = getConfig().getDouble("Abilities.Fire.FireWheel.Speed");
 		this.cooldown = getConfig().getLong("Abilities.Fire.FireWheel.Cooldown");
 		this.fireTicks = getConfig().getDouble("Abilities.Fire.FireWheel.FireTicks");
@@ -130,7 +131,7 @@ public class FireWheel extends FireAbility implements ComboAbility {
 			emitFirebendingLight(tempLoc);
 		}
 
-		for (final Entity entity : GeneralMethods.getEntitiesAroundPoint(this.location, this.radius + 0.5)) {
+		for (final Entity entity : GeneralMethods.getEntitiesAroundPoint(this.location, this.radius * hitboxmultiplier)) {
 			if (entity instanceof LivingEntity && !entity.equals(this.player)) {
 				if (!this.affectedEntities.contains(entity)) {
 					this.affectedEntities.add((LivingEntity) entity);
